1a561df9b374c3bbf9be5ae5e54b6d663a208d60
anildias <anild@qburst.com>
2017-03-13 11:25:55 +0530

    adding project model with relation to user model

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "dashboard#index"
   devise_for :users, controllers: { sessions: 'sessions' }
+
+  resources :projects
 end
